529a8236b25a0174f3a989819de4131f35ddbd6d
Ill-End <120046625+Ill-End@users.noreply.github.com>
2022-12-08 09:20:33 +0100

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('🥑 My Parents New Healthy Diner 🥗')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
+streamlit.text('Kale, Spinach & Rocket Smoothie 🥗')
+streamlit.text('Hard-Boiled Free-Range Egg 🐔')
+streamlit.text('Avacado Toast 🥑 🍞')
